9c5b7260418ddc27385e98ceaf6ee3179c27cbf1
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-17 09:27:33 -0300

    Initial Commit

---
+++ swift-013-stackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-013-stackView
+//
+//  Created by Luiz Carlos da Silva Araujo on 17/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
